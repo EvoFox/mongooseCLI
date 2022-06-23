840ec6dc788c9b63234c291dccde674c770f5039
EvoFox <foulkes.joshua@live.co.uk>
2022-06-23 10:43:18 +0100

    initial commit

---
+++ src/db/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const connection = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log("Successful Connection");
+	} catch (error) {}
+};
