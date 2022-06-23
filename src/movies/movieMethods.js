a32332bef9a806d4206d854e5d54967962cd472b
EvoFox <foulkes.joshua@live.co.uk>
2022-06-23 13:26:12 +0100

    Create Read,Update,Delete functions with Mongoose!

--- src/movies/movieMethods.js
+++ src/movies/movieMethods.js
@@ -8,3 +8,27 @@ exports.addFilm = async (filmObj) => {
 		console.log(error);
 	}
 };
+
+exports.listFilm = async () => {
+	try {
+		return await Film.find({});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.updateFilm = async (criteria, filmObj) => {
+	try {
+		await Film.updateOne(criteria, filmObj);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.deleteFilm = async (criteria) => {
+	try {
+		await Film.deleteOne(criteria);
+	} catch (error) {
+		console.log(error);
+	}
+};
