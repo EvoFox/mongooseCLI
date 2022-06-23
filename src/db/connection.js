3af284a3bb28a47f53ae5baed0afdce34541b3f5
EvoFox <foulkes.joshua@live.co.uk>
2022-06-23 11:25:09 +0100

    created connection file

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,15 @@
+// Import libraries for mongoose and dotenv
 const mongoose = require("mongoose");
-
 require("dotenv").config();
 
 const connection = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_URI);
-		console.log("Successful Connection");
+		console.log("Successful database connection");
 	} catch (error) {}
 };
+
+if (process.env.DEBUG == "true") {
+	connection();
+}
+module.export = connection;
